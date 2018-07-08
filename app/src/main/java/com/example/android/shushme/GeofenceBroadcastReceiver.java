f2dcc6c6ffdf2aa6c1e17bc7a87b12bf2a37b772
Eduardo Przysiezny <eduardo_thp@yahoo.com.br>
2018-07-08 14:34:16 -0500

    05-Exercise completion

--- app/src/main/java/com/example/android/shushme/GeofenceBroadcastReceiver.java
+++ app/src/main/java/com/example/android/shushme/GeofenceBroadcastReceiver.java
@@ -16,10 +16,19 @@ package com.example.android.shushme;
 * limitations under the License.
 */
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.AudioManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
 
 // DONE (4) Create a GeofenceBroadcastReceiver class that extends BroadcastReceiver and override
 // onReceive() to simply log a message when called. Don't forget to add a receiver tag in the Manifest
@@ -40,13 +49,127 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.d(LOG_TAG, "Geofence event (transition) received");
 
-        // TODO (4) Use GeofencingEvent.fromIntent to retrieve the GeofencingEvent that caused the transition
+        // DONE (4) Use GeofencingEvent.fromIntent to retrieve the GeofencingEvent that caused the transition
+        // Get the Geofence Event from the Intent sent through
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+            Log.e(LOG_TAG, String.format("Error code : %d", geofencingEvent.getErrorCode()));
+            return;
+        }
 
-        // TODO (5) Call getGeofenceTransition to get the transition type and use AudioManager to set the
-        // phone ringer mode based on the transition type. Feel free to create a helper method (setRingerMode)
+        // Get the transition type.
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+        // Check which transition type has triggered this event
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            setRingerMode(context, AudioManager.RINGER_MODE_SILENT);
+        } else if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+            setRingerMode(context, AudioManager.RINGER_MODE_NORMAL);
+        } else {
+            // Log the error.
+            Log.e(LOG_TAG, String.format("Unknown transition : %d", geofenceTransition));
+            // No need to do anything else
+            return;
+        }
+        // Send the notification
+        sendNotification(context, geofenceTransition);
+    }
 
-        // TODO (6) Show a notification to alert the user that the ringer mode has changed.
-        // Feel free to create a helper method (sendNotification)
+    // DONE (5) Call getGeofenceTransition to get the transition type and use AudioManager to set the
+    // phone ringer mode based on the transition type. Feel free to create a helper method (setRingerMode)
+
+    /**
+     * Changes the ringer mode on the device to either silent or back to normal
+     *
+     * @param context The context to access AUDIO_SERVICE
+     * @param mode    The desired mode to switch device to, can be AudioManager.RINGER_MODE_SILENT or
+     *                AudioManager.RINGER_MODE_NORMAL
+     */
+    private void setRingerMode(Context context, int mode) {
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        // Check for DND permissions for API 24+
+        if (android.os.Build.VERSION.SDK_INT < 24 ||
+                (android.os.Build.VERSION.SDK_INT >= 24 && !nm.isNotificationPolicyAccessGranted())) {
+            AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            // audioManager.setRingerMode(mode);
+
+            // HAD AN EXCEPTION: The permission request is probably not working :-/
+//            Process: com.example.android.shushme, PID: 29205
+//            java.lang.RuntimeException: Unable to start receiver com.example.android.shushme.GeofenceBroadcastReceiver: java.lang.SecurityException: Not allowed to change Do Not Disturb state
+//            at android.app.ActivityThread.handleReceiver(ActivityThread.java:3047)
+//            at android.app.ActivityThread.-wrap18(ActivityThread.java)
+//            at android.app.ActivityThread$H.handleMessage(ActivityThread.java:1561)
+//            at android.os.Handler.dispatchMessage(Handler.java:102)
+//            at android.os.Looper.loop(Looper.java:154)
+//            at android.app.ActivityThread.main(ActivityThread.java:6119)
+//            at java.lang.reflect.Method.invoke(Native Method)
+//            at com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:886)
+//            at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:776)
+//            Caused by: java.lang.SecurityException: Not allowed to change Do Not Disturb state
+
+        }
 
     }
+
+
+    // DONE (6) Show a notification to alert the user that the ringer mode has changed.
+    // Feel free to create a helper method (sendNotification)
+
+    /**
+     * Posts a notification in the notification bar when a transition is detected
+     * Uses different icon drawables for different transition types
+     * If the user clicks the notification, control goes to the MainActivity
+     *
+     * @param context        The calling context for building a task stack
+     * @param transitionType The geofence transition type, can be Geofence.GEOFENCE_TRANSITION_ENTER
+     *                       or Geofence.GEOFENCE_TRANSITION_EXIT
+     */
+    private void sendNotification(Context context, int transitionType) {
+        // Create an explicit content Intent that starts the main Activity.
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+
+        // Construct a task stack.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+
+        // Add the main Activity to the task stack as the parent.
+        stackBuilder.addParentStack(MainActivity.class);
+
+        // Push the content Intent onto the stack.
+        stackBuilder.addNextIntent(notificationIntent);
+
+        // Get a PendingIntent containing the entire back stack.
+        PendingIntent notificationPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Get a notification builder
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+        // Check the transition type to display the relevant icon image
+        if (transitionType == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            builder.setSmallIcon(R.drawable.ic_volume_off_white_24dp)
+                    .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
+                            R.drawable.ic_volume_off_white_24dp))
+                    .setContentTitle(context.getString(R.string.silent_mode_activated));
+        } else if (transitionType == Geofence.GEOFENCE_TRANSITION_EXIT) {
+            builder.setSmallIcon(R.drawable.ic_volume_up_white_24dp)
+                    .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
+                            R.drawable.ic_volume_up_white_24dp))
+                    .setContentTitle(context.getString(R.string.back_to_normal));
+        }
+
+        // Continue building the notification
+        builder.setContentText(context.getString(R.string.touch_to_relaunch));
+        builder.setContentIntent(notificationPendingIntent);
+
+        // Dismiss notification once the user touches it.
+        builder.setAutoCancel(true);
+
+        // Get an instance of the Notification manager
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Issue the notification
+        mNotificationManager.notify(0, builder.build());
+    }
+
+
 }
--- app/src/main/java/com/example/android/shushme/MainActivity.java
+++ app/src/main/java/com/example/android/shushme/MainActivity.java
@@ -16,6 +16,7 @@ package com.example.android.shushme;
 * limitations under the License.
 */
 
+import android.app.NotificationManager;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -207,10 +208,25 @@ public class MainActivity extends AppCompatActivity implements
         }
 
 
-        //TODO (3) Initialize ringer permissions checkbox
+        // DONE (3) Initialize ringer permissions checkbox
+        // Initialize ringer permissions checkbox
+        CheckBox ringerPermissions = (CheckBox) findViewById(R.id.ringer_permissions_checkbox);
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Check if the API supports such permission change and check if permission is granted
+        if (android.os.Build.VERSION.SDK_INT >= 24 && !nm.isNotificationPolicyAccessGranted()) {
+            ringerPermissions.setChecked(false);
+        } else {
+            ringerPermissions.setChecked(true);
+            ringerPermissions.setEnabled(false);
+        }
+
     }
 
-    // TODO (2) Implement onRingerPermissionsClicked to launch ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS
+    // DONE (2) Implement onRingerPermissionsClicked to launch ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS
+    public void onRingerPermissionsClicked(View view) {
+        Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+        startActivity(intent);
+    }
 
     // DONE (3) Implement onActivityResult and check that the requestCode is PLACE_PICKER_REQUEST
     // DONE (4) In onActivityResult, use PlacePicker.getPlace to extract the Place ID and insert it into the DB
